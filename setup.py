27ae946a51e11a445b46fb7a990f51ced71ced0f
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-02-09 11:16:30 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
-"""
-Module: setup.py
-Description: This Python source file is part of the Native Python Application, which is a project under the Trilobite Coder Lab.
-
-Author:
-- Name: Michael Gene Brockus (Dreamer)
-- Email: michaelbrockus@gmail.com
-- Website: https://trilobite.code.blog
-
-License: This software is released under the Apache License 2.0. Please refer to the LICENSE file for more details.
-
-Purpose:
-- This Python source file contains the implementation for the Native Python Application.
-- It includes the main logic and functionality required for the application to run.
-- Review and modify this file as needed for your specific project requirements.
-
-For more information on the Native Python Application and the Trilobite Coder Lab project, please refer to the project documentation and website.
-"""
+#
+# ==============================================================================
+# Author: Michael Gene Brockus (Dreamer)
+# Email: michaelbrockus@gmail.com
+# Organization: Fossil Logic
+# Description: 
+#     This file is part of the Fossil Logic project, where innovation meets
+#     excellence in software development. Michael Gene Brockus, also known as
+#     "Dreamer," is a dedicated contributor to this project. For any inquiries,
+#     feel free to contact Michael at michaelbrockus@gmail.com.
+# ==============================================================================
+#
 from setuptools import setup, find_packages
 
 
